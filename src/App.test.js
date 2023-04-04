7c05acc787117fa55e1e90d4c4ef186a7d46caf7
FlorTiscornia <flora.tiscornia@gmail.com>
2023-04-04 17:04:29 +0200

    work in progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+beforeEach(()=>{
+  render(<App />)
+
+});
+
+test('renders logo', () => {
+  const logo = screen.getByRole('img', {name:'Logo Rick and Morty'});
+  expect(logo).toBeInTheDocument();
+});
+
+test('renders title', () => {
+  const title = screen.getByText(/List/i);
+  expect(title).toBeInTheDocument();
 });
---
+++ src/test/AllCharacters.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import AllCharacters from '../components/AllCharacters';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+beforeEach(()=>{
+    render(
+        <Router>
+            <AllCharacters/>
+        </Router>
+    )
+})
+
+test('get characterListImg', async () => {
+    const characterImg = await screen.findByRole('img');
+    expect(characterImg).toBeInTheDocument();
+})
+
+test('get characterListName', async () => {
+    const characterName = await screen.findByText(/Rick Sanchez/i);
+    expect(characterName).toBeInTheDocument();
+})
